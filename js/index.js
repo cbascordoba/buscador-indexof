bca5d287dc4afa25aaee0c508dc163648c79e71f
sebastian cordoba <cbascordoba14@gmail.com>
2020-03-11 14:50:41 -0500

    Se crea una lista de productos, se recorren pasando su valor de nombre a minusculas y se pregunta con indexOf para verificar si se encuenta de acuerdo al texto digitado

--- js/index.js
+++ js/index.js
@@ -1,7 +1,41 @@
+const productos=[
+    {nombre:'Platanos', valor:500},
+    {nombre:'Pera', valor:500},
+    {nombre:'Sandia', valor:500},
+    {nombre:'Melon', valor:500},
+    {nombre:'Banano', valor:500}
+]
 const formulario = document.querySelector('#formulario');
 const boton = document.querySelector('#boton');
+const resultado = document.querySelector('#resultado');
 
 const filtrar = () =>{
-    console.log(formulario.value);
+    // console.log(formulario.value);
+    const texto = formulario.value.toLowerCase();
+    //comenzamos con una lista vacia para que no se concatenen cada vez que 
+    //consultamos el metodo filtrar. 
+    resultado.innerHTML = '';
+
+    for(let producto of productos){
+        let nombre = producto.nombre.toLowerCase();
+
+        if(nombre.indexOf(texto) !== -1){
+            resultado.innerHTML += `
+            <li>${producto.nombre} - Valor: ${producto.valor}</li>
+            `
+        }
+
+    }
+
+    if(resultado.innerHTML === ''){
+
+        resultado.innerHTML += `
+        <li>Producto no encontrado</li>
+        `
+       
+    }
 }
+
 boton.addEventListener('click',filtrar);
+formulario.addEventListener('keyup', filtrar)
+filtrar();
